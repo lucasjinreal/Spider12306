ad79885f23b3ae0ba73a2a25ddd6c3eb9b5bfa61
jinfagang <nicholasjela@gmail.com>
2017-01-02 23:27:55 +0800

    add something

--- spiderscript.py
+++ spiderscript.py
@@ -25,6 +25,8 @@ import requests
 import json
 from pprint import pprint
 from prettytable import PrettyTable
+import datetime
+from datetime import timedelta
 
 
 def cli():
@@ -118,6 +120,29 @@ class TrainCollection(object):
         print(pt)
         print('All available tickets shows above.')
 
+    def check_available_trains(self):
+        """
+        Get available trains.
+        :return:
+        """
+        for train in self.trains:
+            if train[5] != '无' and train[5] != '--':
+                print('Find a ticket of GaoTie! Information: {0} {1} {2} {3}'.format(
+                    train[0], train[2], train[3], train[5]
+                ))
+                print(train)
+            elif train[8] != '无' and train[8] != '--':
+                print('Find a ticket of GaoTie! Information: {0} {1} {2} {3}'.format(
+                    train[0], train[2], train[3], train[5]
+                ))
+                print(train)
+            else:
+                print('Sorry, can not find now, I will try again later.')
+
+
+
+
+
 
 def convert_city_to_code(c):
     city_code_dict = {}
@@ -133,7 +158,83 @@ def convert_city_to_code(c):
 
 
 def find_available_train():
+    arguments = docopt(__doc__)
+    from_station = convert_city_to_code(arguments['<from>'])
+    to_station = convert_city_to_code(arguments['<to>'])
+    date_str = arguments['<date>']
+    date_date = str_to_date(date_str)
+    today = datetime.datetime.today()
+    date_earliest = date_date - timedelta(days=3)
+    if date_earliest < today:
+        date_earliest = today
+        print('\33[30;42m --------Start Autopilot Check Available Tickets.--------- \33[0m')
+        for i in range(1, 10):
+            date_next = date_earliest + timedelta(days=1)
+            date_earliest = date_next
+            date_next_str = date_to_str(date_next)
+            from_station = convert_city_to_code(arguments['<from>'])
+            to_station = convert_city_to_code(arguments['<to>'])
+            if not from_station:
+                print('Departure wrong, no such station.')
+            elif not to_station:
+                print('Destination wrong, no such station.')
+            else:
+                url = ('https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date={0}&leftTicketDTO.'
+                       'from_station={1}&leftTicketDTO.to_station={2}&purpose_codes=ADULT'.format(date_next_str,
+                                                                                                  from_station,
+                                                                                                  to_station))
 
+                print('\33[32;41m Check the {0} day on {1} from {2} to {3} \33[0m'.format(i, date_next_str,
+                                                                                          from_station, to_station))
+                r = requests.get(url, verify=False)
+                status = r.json()['status']
+                data = r.json()['data']
+                if status:
+                    all_available_trains = []
+                    trains = TrainCollection(data)
+                    for train in trains:
+                        print(train)
+                else:
+                    print('Request failed. Please check internet connect.')
+    else:
+        print('\33[30;42m --------Start Autopilot Check Available Tickets.--------- \33[0m')
+        for i in range(1, 10):
+            date_next = date_earliest + timedelta(days=1)
+            date_earliest = date_next
+            date_next_str = date_to_str(date_next)
+            from_station = convert_city_to_code(arguments['<from>'])
+            to_station = convert_city_to_code(arguments['<to>'])
+            if not from_station:
+                print('Departure wrong, no such station.')
+            elif not to_station:
+                print('Destination wrong, no such station.')
+            else:
+                url = ('https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date={0}&leftTicketDTO.'
+                       'from_station={1}&leftTicketDTO.to_station={2}&purpose_codes=ADULT'.format(date_next_str,
+                                                                                                  from_station,
+                                                                                                  to_station))
+                print('\33[37;40m Check the {0}th day on {1} from {2} to {3} \33[0m'.format(i, date_next_str,
+                                                                                            from_station, to_station))
+                r = requests.get(url, verify=False)
+                status = r.json()['status']
+                data = r.json()['data']
+                if status:
+                    all_available_trains = []
+                    trains = TrainCollection(data)
+                    trains.check_available_trains()
+
+                else:
+                    print('Request failed. Please check internet connect.')
+
+
+
+def str_to_date(string):
+    return datetime.datetime.strptime(string, '%Y-%m-%d')
+
+
+def date_to_str(date):
+    return datetime.datetime.strftime(date, "%Y-%m-%d")
 
 if __name__ == '__main__':
-    cli()
+    # cli()
+    find_available_train()
--- test.py
+++ test.py
@@ -1,11 +1,28 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-test
+testtidme
 http://www.lewisjin.coding.me
 ~~~~~~~~~~~~~~~
 This script implement by Jin Fagang.
 : copyright: (c) 2017 Didi-Chuxing.
 : license: Apache2.0, see LICENSE for more details.
 """
-print('\33[32;41m info \33[0m')
+from datetime import timedelta, date
+import datetime
+
+a = '01'
+today = datetime.datetime.today()
+day = '2016-01-22'
+
+
+def str_to_date(string):
+    return datetime.datetime.strptime(string, '%Y-%m-%d')
+
+
+def date_to_str(date):
+    return datetime.date.strftime(date, "%Y-%m-%d")
+
+day_date = str_to_date(day)
+print(day_date > today)
+print(date_to_str(day_date))
